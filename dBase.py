a3811fc90bd6f2cbcdcc7265e9aae371bbf6402e
Ковалев Денис Александрович <kovalev_d@bolid.ru>
2019-09-20 19:16:54 +0300

    Changed func table_filling - now filling is correct

--- dBase.py
+++ dBase.py
@@ -1,8 +1,6 @@
 import sqlite3
 import datetime
 
-first_time_1 = True
-first_time_2 = True
 
 def DataBase():
     con = sqlite3.connect('Temperature.db')
@@ -16,27 +14,32 @@ def table_filling(tempInfo):
     con = sqlite3.connect('Temperature.db')
     cursor = con.cursor()
 
-    
     if tempInfo["Sensor"] == 1:
-        if first_time_1 == True:
-            index_for_sensor_1 = 0
-            first_time_1 = False
-        
-        index_for_sensor_1 += 1
+        sql = "SELECT * FROM Sensor_1 WHERE ID=?"
+        cursor.execute(sql, (1,))
+        if cursor.fetchone() == None:
+            index_for_sensor_1 = 1
+        else:
+            cursor.execute("SELECT * FROM Sensor_1 ORDER BY ID DESC LIMIT 1")
+            string_with_value = cursor.fetchone()
+            index_for_sensor_1 = string_with_value[0] + 1
         info_sensor_1 = [index_for_sensor_1, datetime.datetime.now(), tempInfo["Temperature"]]
         cursor.execute('INSERT INTO Sensor_1 VALUES (?,?,?)', info_sensor_1)
     
     elif tempInfo["Sensor"] == 2:
-        if first_time_2 == True:
-            index_for_sensor_2 = 0
-            first_time_2 = False
-        
-        index_for_sensor_2 += 1
+        sql = "SELECT * FROM Sensor_2 WHERE ID=?"
+        cursor.execute(sql, (1,))
+        if cursor.fetchone() == None:
+            index_for_sensor_2 = 1
+        else:
+            cursor.execute("SELECT * FROM Sensor_2 ORDER BY ID DESC LIMIT 1")
+            string_with_value = cursor.fetchone()
+            index_for_sensor_2 = string_with_value[0] + 1
         info_sensor_2 = [index_for_sensor_2, datetime.datetime.now(), tempInfo["Temperature"]]
         cursor.execute('INSERT INTO Sensor_2 VALUES (?,?,?)', info_sensor_2)
 
     else:
-        return 0
+        exit(0)
     
     
     
